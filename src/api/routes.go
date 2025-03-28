7f11bf1727685ce372801edc5857ca433ced6623
deriifirgiawan <derifirgiawan025@gmail.com>
2025-03-28 15:26:23 +0700

    feat: allow all cors

--- src/api/routes.go
+++ src/api/routes.go
@@ -7,12 +7,22 @@ import (
 	merchant "rest-app-pos/src/modules/merchant/routes"
 	product "rest-app-pos/src/modules/product/routes"
 	transaction "rest-app-pos/src/modules/transaction/routes"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(app *containers.AppContainer) *gin.Engine {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true, // Allow requests from any domain
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	api := r.Group("/api")
 	api.GET("/ping", controllers.Ping)
